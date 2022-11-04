011f2dc4384f976891838fb79e6da4202dd1bc77
Diego Dromer <diego.dromer@hotmail.com>
2022-11-03 21:56:52 -0300

    Erro no rebuild, após configurar keys da api da marvel

---
+++ core/src/main/java/com/diegolima/core/data/network/response/DataContainerResponse.kt
@@ -0,0 +1,5 @@
+package com.diegolima.core.data.network.response
+
+data class DataContainerResponse (
+    val result: List<CharacterResponse>
+)
---
+++ core/src/main/java/com/diegolima/core/data/network/response/ThumbnailResponse.kt
@@ -0,0 +1,6 @@
+package com.diegolima.core.data.network.response
+
+data class ThumbnailResponse(
+    val path: String,
+    val extensionPath: String
+)
